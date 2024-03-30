3952c7d4cff023269938a841024018bdada84dba
Jacob Wachspress <jacobwachspress@gmail.com>
2024-03-29 21:45:01 -0400

    heroic PR to Gurobi.jl 2 days ago improves performance adding columns

--- environment.jl
+++ environment.jl
@@ -1,6 +1,6 @@
 import Pkg;
 Pkg.add("DataFrames")
-Pkg.add(name="Gurobi", version="1.2.1")
+Pkg.add(name="Gurobi", version="1.2.3")
 Pkg.add("CSV")
 Pkg.add("DelimitedFiles")
 Pkg.add(name="JuMP", version="1")

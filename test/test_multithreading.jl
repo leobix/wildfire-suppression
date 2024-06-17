592f1a31f7e72893534cdaf52cd5723666f5d940
Jacob Wachspress <jacobwachspress@gmail.com>
2024-06-17 15:03:28 -0400

    profile multithreading

--- test/test_multithreading.jl
+++ test/test_multithreading.jl
@@ -1,24 +1,30 @@
+using Profile
+
 # test multithreading
 Threads.@threads for i ∈ 1:100
-    println(Threads.threadid())
-    j = 0.0
-    for k ∈ 1:10000
-        for l ∈ 1:1000
-            j += (l+k)^0.5
-        end
-    end
+	println(Threads.threadid())
+	j = 0.0
+	for k ∈ 1:10000
+		for l ∈ 1:1000
+			j += (l + k)^0.5
+		end
+	end
 end
 
 println()
 println("again")
 println()
 
-Threads.@threads for i ∈ 1:100
-    println(Threads.threadid())
-    j = 0.0
-    for k ∈ 1:10000
-        for l ∈ 1:1000
-            j += (l+k)^0.5
-        end
-    end
+Profile.init()
+@profile begin
+	Threads.@threads for i ∈ 1:100
+		println(Threads.threadid())
+		j = 0.0
+		for k ∈ 1:10000
+			for l ∈ 1:1000
+				j += (l + k)^0.5
+			end
+		end
+	end
 end
+Profile.print(mincount = 5)

956676df1c2f212e6c4c5fe4ce1b734e7e2d7ae8
Jacob Wachspress <jacobwachspress@gmail.com>
2024-06-17 11:14:18 -0400

    remove old files

--- old_code_to_deprecate/precompile.jl
+++
@@ -1,7 +0,0 @@
-using PackageCompiler, Libdl
-
-PackageCompiler.create_sysimage(
-    ["JuMP", "Gurobi"],
-    sysimage_path = "project_image." * Libdl.dlext,
-    precompile_execution_file = "Main.jl",
-)

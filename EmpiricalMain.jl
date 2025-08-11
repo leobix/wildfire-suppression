9a58e1116d12f2493ba4e13011e3f1ba161b36df
Léonard Boussioux <leobix@mit.edu>
2025-08-11 22:07:44 +0200

    Compute crew count from GACC selection

--- EmpiricalMain.jl
+++ EmpiricalMain.jl
@@ -97,8 +97,8 @@ if args["debug"] == true
 else
 	global_logger(ConsoleLogger(io, Logging.Info, show_limited = false))
 end
-num_fires = 14	
-num_crews = 12
+num_fires = 14
+num_crews = 0
 num_time_periods = 14
 travel_speed = 40.0 * 6.0
 GC.gc()
@@ -114,6 +114,8 @@ crew_routes, fire_plans, crew_models, fire_models, cut_data, init_info = initial
         firefighters_per_crew = firefighters_per_crew,
         fires_by_gacc = fires_by_gacc,
 )
+
+num_crews = length(crew_models)
 
 @info "Total crews" num_crews
 @info "Fire selection criterion" init_info.selection
